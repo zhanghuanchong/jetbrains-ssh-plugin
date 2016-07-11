7a077996813ba5a2277e46485bfdac8f8b4f8bb2
Hans Zhang <devwingcom@gmail.com>
2016-07-12 00:57:55 +0800

    Added first Action from tool menu.

---
+++ src/SuperSSHAction.java
@@ -0,0 +1,18 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * Default tool action to open the tool window.
+ */
+public class SuperSSHAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        String txt= Messages.showInputDialog(project, "What is your name?", "Input Your Name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
+}
